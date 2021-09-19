1a4c8b03bcb7044c213250cb2e5b91689169d416
Muhammad Adnan <79781004+Adnan-Swati@users.noreply.github.com>
2021-09-19 09:39:09 -0700

    Add files via upload

---
+++ assignment no 2.dart
@@ -0,0 +1,35 @@
+void main() {
+//   Answer No 2 
+//   var a = 2;
+//   var b = 1;
+//   var result = --a - --b + ++b + b--;
+//   print (result);
+//   var c = --a;
+//   print (c);
+//   var d =--a - --b ;
+//   print (d);
+//   var j = --a - --b + ++b ;
+//   print (j);
+//   var f = --a - --b  + ++b + b--;
+//   print (f);
+  
+// //   Answer No 3
+  
+//   var cost = 600 ;
+//   var tickets = 5;
+//   var product = cost *tickets;
+//   print ( "${product} PKR");
+  
+  
+
+//   print([ 1,2,3,4,5,6,7].where((e) => ![3,5,6,7,9,10].contains(e)).toList());
+  
+// Answer No 7
+   
+//   var lst = [7,7,7,7,7,7,7,7,7,7];
+//   for( var i=1; i<=10; i++){
+//     print ( "${7} * ${i} = ${7*i}");
+    
+    
+  } 
+}
